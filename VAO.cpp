f88acd847d9de325428c1022a2cd6b40c63df545
SponkeDa <sponkeda@gmail.com>
2026-06-19 19:55:13 -0700

    Done with organizing
    Organizing VAO, VBO, EBO. Next Next is Fix Color

---
+++ VAO.cpp
@@ -0,0 +1,30 @@
+#include "VAO.h"
+
+VAO::VAO()
+{
+	glGenVertexArrays(1, &ID);
+}
+
+void VAO::LinkVBO(VBO VBO, GLuint layout)
+{
+	VBO.Bind();
+	glBindVertexArray(ID);
+	glVertexAttribPointer(layout, 3, GL_FLOAT, GL_FALSE, 0 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(layout);
+	VBO.Unbind();
+}
+
+void VAO::Bind()
+{
+	glBindVertexArray(ID);
+}
+
+void VAO::Unbind()
+{
+	glBindVertexArray(0);
+}
+
+void VAO::Delete()
+{
+	glDeleteVertexArrays(1, &ID);
+}
--- main.cpp
+++ main.cpp
@@ -2,19 +2,11 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = vec4(aPos, 1.0);\n"
-"}\0";
+#include "shaderClass.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
 
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"    FragColor = vec4(1.0f, 0.5f, 0.02f, 1.0f);\n"
-"}\0";
 
 int main() 
 {
@@ -60,66 +52,37 @@ int main()
 	// Set the viewport to cover the new window
 	glViewport(0, 0, 800, 800);
 
-	// Build and compile our shader program
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	Shader shaderProgram("default.vert", "default.frag");
 
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	
-	// Check for shader compile errors
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	VAO VAO1;
+	VAO1.Bind();
 
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	
-	// Link shaders
-	GLuint shaderProgram = glCreateProgram();
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
 
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	VAO1.LinkVBO(VBO1, 0);
 
-
-	GLuint VAO, VBO, EBO;
-	
-	// Generate and bind the Vertex Array Object (VAO) and Vertex Buffer Object (VBO)
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-
-	// Bind the VAO and VBO, and set the vertex data
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
-	// Upload the vertex data to the GPU
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// Define the vertex attribute pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	// Unbind the VBO, VAO, and EBO to prevent accidental modification
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
 
 	// Set up vertex data and buffers and configure vertex attributes
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
 
+
 	// Main Render loop
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+
 		glClear(GL_COLOR_BUFFER_BIT);
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
+
+		shaderProgram.Activate();
+
+		VAO1.Bind();
+
 		//glDrawArrays(GL_TRIANGLES, 0, 6);
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
@@ -127,10 +90,10 @@ int main()
 		glfwPollEvents();
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
-	glDeleteProgram(shaderProgram);
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
